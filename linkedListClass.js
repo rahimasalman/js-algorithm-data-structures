becc73f570f530055203a510cc330f6e9a5bb378
rahimasalman <rehimesalman7@gmail.com>
2023-08-09 16:18:00 +0400

    add example of linked list: print method implementation

--- linkedListClass.js
+++ linkedListClass.js
@@ -35,12 +35,33 @@ class LinkedList {
         }
         this.size ++;
     }
+
+    print() {
+        if(this.isEmpty()) {
+            console.log('This list is empty')
+        } else {
+            let curr = this.head;
+            let listValues = '';
+            while(curr) {
+                listValues += `${curr.value} `
+                curr = curr.next;
+            }
+            console.log(listValues);
+        }
+    }
 }
 
 const list = new LinkedList();
 console.log('Is list empty?', list.isEmpty());
 console.log('The list size is:', list.getSize());
 
+list.print();
+
 list.prepend(100);
+
+list.print();
+
 list.prepend(200);
 list.prepend(300);
+
+list.print();
