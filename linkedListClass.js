aab0a8780434eeef5a78d7555c440d5988909083
rahimasalman <rehimesalman7@gmail.com>
2023-08-09 15:36:43 +0400

    add example of linked list class: prepend method implementation

--- linkedListClass.js
+++ linkedListClass.js
@@ -4,7 +4,9 @@ class Node {
         this.next = null;
     }
 }
-
+/*
+Linked list class
+ */
 class LinkedList {
     constructor() {
         this.head = null;
@@ -18,8 +20,27 @@ class LinkedList {
     getSize() {
         return this.size;
     }
+
+    /* Linked list - Prepend */
+    prepend(value) {
+        const node = new Node(value);
+        // (Empty list)
+        if(this.isEmpty()) {
+            this.head = node;
+        }
+        // (Existing list)
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size ++;
+    }
 }
 
 const list = new LinkedList();
 console.log('Is list empty?', list.isEmpty());
 console.log('The list size is:', list.getSize());
+
+list.prepend(100);
+list.prepend(200);
+list.prepend(300);
