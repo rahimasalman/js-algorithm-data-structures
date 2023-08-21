26335c9a81dfd27d8aa7e349a036a60fae196dad
rahimasalman <rehimesalman7@gmail.com>
2023-08-21 12:41:25 +0400

    add example of binary search: tree depth first search (dfs) implementation

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -53,17 +53,52 @@ class BinarySearchTree {
             }
         }
     }
+
+    preOrder(root) {
+        if(root) {
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+    inOrder(root) {
+        if(root) {
+            this.inOrder(root.left);
+            console.log(root.value);
+            this.inOrder(root.right);
+        }
+    }
+    postOrder(root) {
+        if(root) {
+            this.postOrder(root.left);
+            this.postOrder(root.right);
+            console.log(root.value);
+        }
+    }
 }
 
 const binarySearchTree = new BinarySearchTree();
 console.log(binarySearchTree.isEmpty());
 
 binarySearchTree.insert(10);
-binarySearchTree.insert(27);
-binarySearchTree.insert(3);
+binarySearchTree.insert(5);
+binarySearchTree.insert(15);
 
 console.log(binarySearchTree.isEmpty());
 
 console.log(binarySearchTree.search(binarySearchTree.root,10));
 console.log(binarySearchTree.search(binarySearchTree.root,5));
 console.log(binarySearchTree.search(binarySearchTree.root,3));
+
+binarySearchTree.insert(3);
+binarySearchTree.insert(7);
+
+binarySearchTree.preOrder(binarySearchTree.root);
+
+console.log('this is inOrder func ------------------')
+
+binarySearchTree.inOrder(binarySearchTree.root);
+
+console.log('this is postOrder func ------------------')
+
+binarySearchTree.postOrder(binarySearchTree.root);
